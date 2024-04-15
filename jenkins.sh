69275f9cabe4ae750d2243439c777e97208f4058
Suman Kumar Mishra <90380803+kmsuman2705@users.noreply.github.com>
2024-04-15 23:36:16 +0530

    jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install fontconfig java-17-openjdk
