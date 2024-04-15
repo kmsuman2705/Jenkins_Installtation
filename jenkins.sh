fb9c4b07440d83797eef400a1405789954e447d3
Suman Kumar Mishra <90380803+kmsuman2705@users.noreply.github.com>
2024-04-15 23:46:57 +0530

    jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+sudo yum update
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-17-openjdk
-sudo amazon-linux-extras install java-openjdk11
-sudo yum install jenkins
-systemctl start jenkins
+sudo yum upgrade
+yum install fontconfig java-17-openjdk -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
