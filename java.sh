8ae1731bbf4d59730e0f4b7992f563d502bc6041
Suman Kumar Mishra <90380803+kmsuman2705@users.noreply.github.com>
2024-04-17 22:52:05 +0530

    java.sh

---
+++ java.sh
@@ -0,0 +1,4 @@
+cd ~
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm"
+
+ sudo rpm -ivh jdk-17_linux-x64_bin.rpm
